d4469f9c889ed2cc3ddc567d313707f40091e6af
Bryan <bryan.guerrero@michelada.io>
2022-07-12 20:29:52 -0500

    Add user moder and login process for it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    user_posts_path(resource)
+  end
+
+  def after_sign_out_path_for(scope)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :posts do
     resources :comments
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
 end
